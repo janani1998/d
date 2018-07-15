3d9c1f7c1edde93980f1c9db246520173029f387
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-15 12:02:15 +0530

    Create evenno.java

---
+++ evenno.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=2,b=10;
+		for(int i=3;i<10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
