79093ab5655d7673bc4b710dcbb552354d47dd6a
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-07-15 11:58:57 +0530

    Create oddno.java

---
+++ oddno.java
@@ -0,0 +1,14 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=1,b=6;
+		for(int i=2;i<6;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
